f77a9c60689224cfcd6fccca389b1a82ce6e38fd
hammu116 <59144819+hammu116@users.noreply.github.com>
2020-03-25 20:50:16 +0530

    Add files via upload

---
+++ tddjunittask/src/main/java/Epam/tddjunittask/App.java
@@ -0,0 +1,20 @@
+package Epam.tddjunittask;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public String Remove(String next) {
+
+		// TODO Auto-generated method stub
+
+		return null;
+
+	}
+}
